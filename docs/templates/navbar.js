ee7855ae1ad2fbfb0ea940ddf257c570ee377f91
James Briggs <james.briggs@hotmail.com>
2020-05-09 16:02:39 +0100

    page fixes

---
+++ docs/templates/navbar.js
@@ -0,0 +1,26 @@
+document.write('\
+\
+<nav class="navbar navbar-expand-lg navbar-dark bg-dark static-top">\
+  <div class="container">\
+    <a class="navbar-brand" href="https://jamescalam.github.io/autodocs/">Main</a>\
+    <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarResponsive" aria-controls="navbarResponsive" aria-expanded="false" aria-label="Toggle navigation">\
+      <span class="navbar-toggler-icon"></span>\
+    </button>\
+    <div class="collapse navbar-collapse" id="navbarResponsive">\
+      <ul class="navbar-nav ml-auto">\
+        <li class="nav-item">\
+          <a class="nav-link" href="https://jamescalam.github.io/docs.html">Docs\
+          </a>\
+        </li>\
+        <li class="nav-item">\
+          <a class="nav-link" href="https://medium.com/@jamescalam">Medium</a>\
+        </li>\
+        <li class="nav-item">\
+          <a class="nav-link" href="https://github.com/jamescalam/autodocs"><i>Autodocs</i></a>\
+        </li>\
+      </ul>\
+    </div>\
+  </div>\
+</nav>\
+\
+');
